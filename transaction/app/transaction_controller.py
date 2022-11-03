49a3b0de22ebd2238e2e08430152d117ab68a42d
xgeagregori <xgeagregori@gmail.com>
2022-11-03 13:46:52 +0000

    [transaction] Add CBV

--- transaction/app/transaction_controller.py
+++ transaction/app/transaction_controller.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from mangum import Mangum
 
-app = FastAPI()
+from fastapi_utils.inferring_router import InferringRouter
+from fastapi_utils.cbv import cbv
+
+app = FastAPI(
+    root_path="/prod/transaction-api/v1",
+    title="Transaction API",
+    version="1.0.0",
+)
+handler = Mangum(app, api_gateway_base_path="/transaction-api/v1")
+router = InferringRouter()
 
 
-@app.get("/transactions")
-async def root():
-    return {"message": "Welcome to the Transaction Service!"}
+@cbv(router)
+class TransactionController:
+    @app.get("/transactions")
+    def get_transactions():
+        """Get transactions."""
+        return {"message": "Welcome to the Transaction Service!"}
+
+
+app.include_router(router)
--- transaction/tests/test_transaction_controller.py
+++ transaction/tests/test_transaction_controller.py
@@ -5,7 +5,7 @@ from app.transaction_controller import app
 client = TestClient(app)
 
 
-def test_read_main():
+def test_get_transactions():
     response = client.get("/transactions")
     assert response.status_code == 200
     assert response.json() == {"message": "Welcome to the Transaction Service!"}
