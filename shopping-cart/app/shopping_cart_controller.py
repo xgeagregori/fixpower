2099d56f5971a20bf55a78d53950d70bb8d9add7
xgeagregori <xgeagregori@gmail.com>
2022-11-03 13:46:28 +0000

    [shopping-cart] Add CBV

--- shopping-cart/app/shopping_cart_controller.py
+++ shopping-cart/app/shopping_cart_controller.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from mangum import Mangum
 
-app = FastAPI()
+from fastapi_utils.inferring_router import InferringRouter
+from fastapi_utils.cbv import cbv
+
+app = FastAPI(
+    root_path="/prod/shopping-cart-api/v1",
+    title="Shopping Cart API",
+    version="1.0.0",
+)
+handler = Mangum(app, api_gateway_base_path="/shopping-cart-api/v1")
+router = InferringRouter()
 
 
-@app.get("/shopping-carts")
-async def root():
-    return {"message": "Welcome to the Shopping Cart Service!"}
+@cbv(router)
+class ShoppingCartController:
+    @app.get("/shopping-carts")
+    def get_shopping_carts():
+        """Get shopping carts."""
+        return {"message": "Welcome to the Shopping Cart Service!"}
+
+
+app.include_router(router)
--- shopping-cart/tests/test_shopping_cart_controller.py
+++ shopping-cart/tests/test_shopping_cart_controller.py
@@ -5,7 +5,7 @@ from app.shopping_cart_controller import app
 client = TestClient(app)
 
 
-def test_read_main():
+def test_get_shopping_carts():
     response = client.get("/shopping-carts")
     assert response.status_code == 200
     assert response.json() == {"message": "Welcome to the Shopping Cart Service!"}
