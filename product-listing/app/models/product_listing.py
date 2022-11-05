35a8e16a1cfc9643bc521707cc7a134999f13780
Yang <d244975154@hotmail.com>
2022-11-05 23:06:07 +0000

    [product_listing] modify model

--- product-listing/app/models/product_listing.py
+++ product-listing/app/models/product_listing.py
@@ -19,10 +19,10 @@ class ProductListingIndex(GlobalSecondaryIndex):
 
 class ProductListing(Model):
     class Meta:
-        table_name = os.getenv("AWS_USER_TABLE_NAME")
+        table_name = os.getenv("AWS_PRODUCT_LISTING_TABLE_NAME")
         aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID_LAMBDA")
         aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY_LAMBDA")
 
     id = UnicodeAttribute(hash_key=True)
     listed_price = NumberAttribute()
-    productListing_index = ProductListingIndex()
+    # productListing_index = ProductListingIndex()
