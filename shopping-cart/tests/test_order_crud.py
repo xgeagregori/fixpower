96cf7ff0915bdf3dc6b12b38b205fde13abb544d
yusufahmad0350 <yusufahmad0350@gmail.com>
2022-11-08 19:18:35 +0000

    [order] add get route

--- shopping-cart/tests/test_order_crud.py
+++ shopping-cart/tests/test_order_crud.py
@@ -16,7 +16,7 @@ class TestSuiteOrderCRUD:
         user_response = requests.post(
             os.getenv("API_GATEWAY_URL") + "/user-api/v1/register",
             json={
-                "username": "testUsernameOrder1",
+                "username": "testUsernameOrder3",
                 "email": "testOrder@example.com",
                 "password": "testPasswordOrder",
             },
@@ -33,6 +33,29 @@ class TestSuiteOrderCRUD:
         assert response.status_code == 201
         assert "id" in response.json()
         ValueStorageOrderCRUD.order_id = response.json()["id"]
+        
+    # def test_create_order_with_existing_id(self):
+    #     response = client.post(
+    #         "/shopping-carts",
+    #         json={"user": {"id": ValueStorageOrderCRUD.user_id}, "price": "10"},
+    #     )
+    #     assert response.status_code == 422
+    #     assert response.json()=={"detail":"Order id already exists"}
+    
+    
+    def test_get_orders(self):
+        response= client.get("/shopping-carts/")
+        assert response.status_code == 200
+        
+    def test_get_order_by_id(self):
+        response= client.get("/shopping-carts/{ValueStorageOrderCRUD.user_id}")
+        assert response.status_code == 200
+        assert "user" in response.json()
+        assert "price" in response.json()
+        assert "created_at" in response.json()
+        
+    
+    
 
     # def test_delete_user(self):
     #     user_response = requests.delete(
