ed4dacd85513baac486205555ef2b5ba2d1124f8
xgeagregori <xgeagregori@gmail.com>
2022-11-19 12:25:42 +0000

    [user] Add payment method to settings

--- user/app/models/settings.py
+++ user/app/models/settings.py
@@ -1,6 +1,7 @@
-from pynamodb.attributes import MapAttribute, BooleanAttribute
+from pynamodb.attributes import MapAttribute, BooleanAttribute, UnicodeAttribute
 
 
 class SettingsAttribute(MapAttribute):
     sms_notifications = BooleanAttribute()
     email_notifications = BooleanAttribute()
+    payment_method = UnicodeAttribute(null=True)
--- user/app/schemas/settings.py
+++ user/app/schemas/settings.py
@@ -1,22 +1,32 @@
 from pydantic import BaseModel
 from typing import Optional
+from enum import Enum
 
+class PaymentMethod(str, Enum):
+    APPLE_PAY = "APPLE_PAY"
+    CREDIT_CARD = "CREDIT_CARD"
+    GOOGLE_PAY = "GOOGLE_PAY"
+    PAYPAL = "PAYPAL"
 
 class SettingsCreate(BaseModel):
     sms_notifications: Optional[bool] = True
     email_notifications: Optional[bool] = True
+    payment_method: Optional[PaymentMethod] = None
 
 
 class SettingsUpdate(BaseModel):
     sms_notifications: Optional[bool]
     email_notifications: Optional[bool]
+    payment_method: Optional[PaymentMethod]
 
 
 class SettingsInDB(BaseModel):
     sms_notifications: bool
     email_notifications: bool
+    payment_method: Optional[PaymentMethod]
 
 
 class SettingsOut(BaseModel):
     sms_notifications: bool
     email_notifications: bool
+    payment_method: Optional[PaymentMethod]
